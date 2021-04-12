3d9faee13951aaef1e2037e33d222e0accd2691f
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2021-04-12 08:33:25 +0200

    Enrich java file

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,18 +3,28 @@ package example;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Example  {
+abstract class Example0
+{
+	
+	
+}
+ //Test comment 0
+public class Example extends Example0  {
+	 //Test comment 1
   private ArrayList<String> names;
 
   public Example() {
+	  //Test comment 2
     names = new ArrayList<>();
   }
 
   public void addName(String name) {
+	  //Test comment 3
     names.add(name);
   }
 
   public List<String> getNames() {
-    return new ArrayList<>(names);
+	  //Test comment 4
+    return new ArrayList<>(names); //another inline comment
   }
 }
