d3691f79be1890579a22f119910061eb8672a05a
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2021-04-14 12:07:20 +0200

    more structures

--- HelloWorld.java
+++ HelloWorld.java
@@ -24,28 +24,178 @@ import org.eclipse.jdt.core.dom.SwitchStatement;
 import org.eclipse.jdt.core.dom.TagElement;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 
-public class HelloWorld {
 
+interface Drawable{  
+    public void draw();  
+}  
+
+interface Addable{  
+    int add(int a,int b);  
+}  
+
+public class HelloWorld {
+	int count = 0;
+	
+	public static void test(String[] args) {  
+        int width=10;  
+  
+        //without lambda, Drawable implementation using anonymous class  
+        Drawable d=new Drawable(){  
+            public void draw(){System.out.println("Drawing "+width);}  
+        };  
+        d.draw();  
+    }  
+	
+	 public static void test2(String[] args) {  
+	        int width=10;  
+	          
+	        //with lambda  
+	        Drawable d2=()->{  
+	            System.out.println("Drawing "+width);  
+	        };  
+	        d2.draw();  
+	    }  
+	 
+	 public static void test3(String[] args) {  
+         
+	        // Multiple parameters in lambda expression  
+	        Addable ad1=(a,b)->(a+b);  
+	        System.out.println(ad1.add(10,20));  
+	          
+	        // Multiple parameters with data type in lambda expression  
+	        Addable ad2=(int a,int b)->(a+b);  
+	        System.out.println(ad2.add(100,200));  
+	    }  
+	 
+	 public static void test4(String[] args) {  
+         
+	        List<String> list=new ArrayList<String>();  
+	        list.add("ankit");  
+	        list.add("mayank");  
+	        list.add("irfan");  
+	        list.add("jai");  
+	          
+	        list.forEach(  
+	            (n)->System.out.println(n)  
+	        );  
+	    }  
+	 
+	 public static void test5(String[ ] args) {
+		    try {
+		      int[] myNumbers = {1, 2, 3};
+		      System.out.println(myNumbers[10]);
+		    } catch (Exception e) {
+		      System.out.println("Something went wrong.");
+		    }
+		  }
+	 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("holi");
+		
+		int testIf = 2;
+		if (testIf == returntwo())
+		{
 			
+			System.out.println("Este es el if");
+		}
+		else
+		{
+			System.out.println("Este es el else");			
+		}
+		
+		if (testIf == returntwo())
+		{
 			
-			ASTParser parser = ASTParser.newParser(AST.JLS10);
-			parser.setSource("package example;\r\n import java.util.ArrayList;\r\n import java.util.List;\r\n public class Example {\r\n   public Example() {\r\n  names = new ArrayList<>();\r\n  }\r\n \r\n  public void addName(String name) {\r\n  names.add(name);\r\n  }\r\n \r\n  public List<String> getNames() {\r\n  return new ArrayList<>(names);\r\n   }\r\n }\r\n ".toCharArray());
-			//parser.setSource("/*abc*/".toCharArray());
-			parser.setKind(ASTParser.K_COMPILATION_UNIT);
-			ASTNode node = parser.createAST(null);
+			System.out.println("Este es el if 2");
+		}
+		else if (testIf == 3)
+		{
+			System.out.println("Este es el elseif");			
+		}
+		
+		while(testIf < 5)
+		{
+			System.out.println("Este es el while");
+			testIf++;
+		}
+		
+		do
+		{
+			System.out.println("Este es el do while");
+			testIf++;
 			
-			print(node);
-	 
-			//final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+			if (testIf == 1)
+			{
+				break;
+			}
+			else
+			{
+				continue;
+			}			
+		}
+		while (testIf < 50);
+		
+		int i = (int) (Math.random() * 5.0);
+        switch (i) {
+	        case 0:
+	            System.out.println("Zero");
+	            break;
+	
+	        case 1:
+	            System.out.println("One");
+	            break;
+	
+	        case 2:
+	        case 3:
+	            System.out.println("Two or Three");
+	            break;
+	
+	        default:
+	            System.out.println("Other");
+	            break;
+            
+        }
+        
+        
+		System.out.println("holi");
+		
+		
+		ASTParser parser = ASTParser.newParser(AST.JLS10);
+		parser.setSource("package example;\r\n import java.util.ArrayList;\r\n import java.util.List;\r\n public class Example {\r\n   public Example() {\r\n  names = new ArrayList<>();\r\n  }\r\n \r\n  public void addName(String name) {\r\n  names.add(name);\r\n  }\r\n \r\n  public List<String> getNames() {\r\n  return new ArrayList<>(names);\r\n   }\r\n }\r\n ".toCharArray());
+		//parser.setSource("/*abc*/".toCharArray());
+		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+		ASTNode node = parser.createAST(null);
+		
+		print(node);
+ 
+		//final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
 			
 	 /* esto es un comentario
 	   bastante fenomenal
 	  */
 			
 		
+	}
+	
+	public void syncadd(int value){
+
+	    synchronized(this){
+	       this.count += value;   
+	    }
+	  }
+	
+	 public synchronized void add(int value){
+	      this.count += value;
+	  }
+	
+	static private int givemeatwo()
+	{
+		return 2;		
+	}
+	
+	static private int returntwo()
+	{
+		return givemeatwo();		
 	}
 	
 	static private void print(ASTNode node) {
